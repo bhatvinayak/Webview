197b08c9a360c2b26f2d37c189f9ecdbefef0598
B G Vinayak <bhatvinayak94@gmail.com>
2019-10-02 21:25:53 +0530

    webview in app

---
+++ app/src/main/java/com/bgvinayak/webview/MainActivity.java
@@ -0,0 +1,50 @@
+package com.bgvinayak.webview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btn;
+    WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn = (Button) findViewById(R.id.button);
+
+        webView = (WebView) findViewById(R.id.wv);
+        webView.setWebViewClient(new MyBrowser());
+        webView.setVisibility(View.GONE);
+
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = "https://google.com";
+
+                webView.setVisibility(View.VISIBLE);
+
+                webView.getSettings().setLoadsImagesAutomatically(true);
+                webView.getSettings().setJavaScriptEnabled(true);
+                webView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+                webView.loadUrl(url);
+            }
+        });
+    }
+
+    private class MyBrowser extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+    }
+}
